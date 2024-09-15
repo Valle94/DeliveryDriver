11f68435c8643eef3e9bdef1955b845a3632adee
Max Valle <55932182+Valle94@users.noreply.github.com>
2024-09-15 01:22:11 -0400

    Progress through 2.20 of "Delivery Driver" Tutorial
    Completed all required work included in the "Delivery Driver" tutorials. Also added a fuel system for extra credit.

--- Delivery Driver/Assets/Scripts/Driver.cs
+++ Delivery Driver/Assets/Scripts/Driver.cs
@@ -1,17 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Driver : MonoBehaviour
 {
-    //Base steer and movespeeds
+    //Starting Variables
     [SerializeField] float steerSpeed = 600f;
-    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 10f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float emptySpeed = 0.2f;
+    [SerializeField] float fuel = 100f;
+    [SerializeField] float fuelBurnRate = 20f;
+    [SerializeField] Slider fuelSlider;
+
+    //Boost/Slow timer variable
+    float timer = 0;
+
+    //Current fuel
+    float currentFuel;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentFuel = fuel;
+    }
+
+    //slow trigger
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            moveSpeed = slowSpeed;
+            //Slow down for this amount of seconds
+            timer = 2;
+        }
+    }
+
+    //Boost trigger
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.tag == "Boost")
+        {
+            //Optional destroy boost on pickup:
+            //Destroy(other.gameObject);
+            moveSpeed = boostSpeed;
+            //Boost for this amount of seconds
+            timer = 3;
+        }
+        if (other.tag == "Fuel")
+        {
+            currentFuel += 15;
+        }
     }
 
     // Update is called once per frame
@@ -21,5 +63,32 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+
+        //boost/slow timer iteration
+        timer -= Time.deltaTime;
+
+        //When boost timer is up, return speed to normal
+        if (timer <=0)
+        {
+            moveSpeed = 20f;
+        }
+
+        //Decrease fuel if moving
+        if (Input.GetAxis("Horizontal") != 0 | Input.GetAxis("Vertical") != 0)
+        {
+            currentFuel -= fuelBurnRate * Time.deltaTime;
+        }
+
+        //Slow to a crawl if fuel is empty
+        if (currentFuel <= 0)
+        {
+            currentFuel = 0;
+        }
+        if (currentFuel == 0)
+        {
+            moveSpeed = moveSpeed * emptySpeed;
+        }
+        //Adjust fuel slider based on current fuel
+        fuelSlider.value = currentFuel / fuel;
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # DeliveryDriver
 
+**Changelog 09/14/24**
+- Finished all "Delivery Driver" Lessons
+- Set camera to follow the Driver as they move around the map
+- Used "ifs" and bools to provide logic for changing color when packages are picked up
+- Added triggered boosts and collision penalties that alter the player speed for a few seconds
+- Added a fuel system with UI elements. Fuel drains as the player moves, and if the tank is empty, speed is reduced significantly
+- Fuel can be picked up using collision triggers as well
+
+**To do**
+- Create Tutorial level that leads to main level up completion
+
 Changelog 09/13/24:
 - Progressed through lesson 2.13
 - Added and resized assets to fit the game environment
